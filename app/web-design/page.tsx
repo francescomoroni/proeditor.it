a8d4246d240776109e05b67d8fcecebb33ddcc82
francesco.moroni <francesco.moroni@lbaconsulting.eu>
2024-12-27 14:01:12 +0100

    added metadata

--- app/web-design/page.tsx
+++ app/web-design/page.tsx
@@ -7,19 +7,31 @@ import { Testimonials } from "@/components/web-design/Testimonials";
 import { Empathy } from "@/components/web-design/Empathy";
 import { ContactFormWebDesign } from "@/components/web-design/ContactForm";
 import { WhyItWorks } from "@/components/web-design/WhyItWorks";
+import Head from "next/head";
 
 export default function WebDesignerPage() {
+  const metadata = {
+    title: "Web Design | Pro Editors",
+    description:
+      "Sviluppo siti web professionali per piccole attività, liberi professionisti e imprenditori come te. Porta la tua attività online con un sito web professionale a soli €29.90 al mese.",
+  };
   return (
-    <div className="min-h-screen bg-white">
-      <Hero />
-      <Empathy />
-      <Benefits />
-      <Pricing />
-      <WhyItWorks />
-      <Testimonials />
-      <HowItWorks />
-      <ContactFormWebDesign />
-    </div>
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Aggiungi altri meta tag SEO se necessario */}
+      </Head>
+      <div className="min-h-screen bg-white">
+        <Hero />
+        <Empathy />
+        <Benefits />
+        <Pricing />
+        <WhyItWorks />
+        <Testimonials />
+        <HowItWorks />
+        <ContactFormWebDesign />
+      </div>
+    </>
   );
-};
-
+}
